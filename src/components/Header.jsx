3f05dc030388e8d163de59e20a9992d1e0547a65
Subeg <84019079+subegshahi@users.noreply.github.com>
2022-11-27 00:01:54 +0545

    second commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import navLogo from "../assets/images/chitrawan-khabar-logo.png";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className=" text-gray-800 border-b border-gray-200">
+      <div className="container mx-auto px-32 py-6 flex justify-between items-center">
+        <a href="#">
+          <img className="w-44" src={navLogo} alt="" />
+        </a>
+
+        <div className="space-x-10">
+          <a className="font-semibold " href="#">
+            Home
+          </a>
+          <a className="font-semibold " href="#">
+            Fixtures
+          </a>
+          <a className="font-semibold " href="#">
+            News
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export const Main = () => {
-  return <div>Main</div>;
+  return (
+    <main>
+      <div className="container mx-auto px-32 mt-10">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptates
+          possimus iusto vero voluptatem doloribus minima officiis non ullam et
+          facere aut aliquid accusantium ipsa, cupiditate repellendus reiciendis
+          aperiam provident soluta numquam! Labore provident nostrum esse
+          tenetur incidunt ratione sequi, fugit magnam illum itaque ipsa sint
+          dolor voluptate eligendi rerum!
+        </p>
+      </div>
+    </main>
+  );
 };
