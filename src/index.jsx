559d3428058f812f365d74e89e2ed7a4c19666d6
Subeg <84019079+subegshahi@users.noreply.github.com>
2022-11-29 21:39:42 +0545

    Added new components

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
+import { Footer } from "./components/Footer";
+import { Login } from "./components/Login";
+import { Signup } from "./components/Signup";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
     <Main />
+    <Footer />
+    <Login />
+    <Signup />
   </React.StrictMode>
 );
