27721a0cbc955bd8a0ea44153b6eee254b7f7fa7
Subeg <84019079+subegshahi@users.noreply.github.com>
2022-12-02 23:16:01 +0545

    Responsive for desktop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import worldCup from "../assets/icons/world-cup.svg";
+import wc2022Logo from "../assets/icons/wc2022-logo.svg";
 
 export const Home = () => {
   return (
-    <section className="text-gray-800 ">
-      <div className="container mx-auto px-5 lg:px-32">
-        <div className="border rounded-lg p-10 mt-20 ">
+    <section>
+      <div className="container mx-auto px-5 mt-20 lg:flex lg:px-32">
+        <div className="border rounded-lg p-10 mt-20 lg:w-1/2">
           <div className="flex flex-col items-center">
             <img className="w-40" src={worldCup} alt="" />
 
@@ -23,6 +24,12 @@ export const Home = () => {
             </button>
           </form>
         </div>
+
+        <img
+          className="w-full aspect-auto mt-10 lg:w-1/2 lg:-mt-5"
+          src={wc2022Logo}
+          alt=""
+        />
       </div>
     </section>
   );
--- src/pages/Predict.jsx
+++ src/pages/Predict.jsx
@@ -13,81 +13,82 @@ export const Predict = () => {
   return (
     <main>
       <div className="container mx-auto px-5 mt-20 lg:flex lg:px-32">
-        <div className="border border-gray-300 rounded-md p-10 lg:w-1/2">
-          <form action="">
+        <form
+          className="border border-gray-300 rounded-md p-10 lg:w-1/2"
+          action=""
+        >
+          <div>
             <div>
-              <div>
-                <div className="flex items-center ">
-                  <img className="w-10" src={cup} alt="" />
-                  <p className="font-bold text-lg text-gray-800">
-                    World Cup 2022 - Round of 16
-                  </p>
-                </div>
-                <hr className="mt-3 border-gray-300" />
+              <div className="flex items-center ">
+                <img className="w-10" src={cup} alt="" />
+                <p className="font-bold text-lg text-gray-800">
+                  World Cup 2022 - Round of 16
+                </p>
+              </div>
+              <hr className="mt-3 border-gray-300" />
+            </div>
+
+            <p className="mt-10 text-center text-3xl font-bold tracking-wide text-gray-800">
+              Who will win?
+            </p>
+
+            <div className="flex gap-5 mt-10">
+              <div className="border border-gray-300  rounded-md p-5 flex flex-col gap-5 items-center w-1/2">
+                <img
+                  className="w-20 h-20 object-cover object-center rounded-full"
+                  src={netherlands}
+                  alt=""
+                />
+
+                {/* <p className="text-lg font-semibold">Netherlands</p> */}
+
+                <button className="bg-red-300 text-red-700 font-semibold tracking-wider px-3 py-1 rounded-full text-center flex justify-center items-center hover:bg-red-400 hover:text-red-900">
+                  Netherlands
+                </button>
               </div>
 
-              <p className="mt-10 text-center text-3xl font-bold tracking-wide text-gray-800">
-                Who will win?
+              {/* <div className="w-20 h-20 rounded-full flex justify-center items-center font-semibold text-xl border shadow-sm">
+                X
+              </div> */}
+
+              <div className="border border-gray-300 rounded-md p-5 flex flex-col gap-5 items-center w-1/2">
+                <img
+                  className="w-20 h-20 object-cover object-center rounded-full"
+                  src={usa}
+                  alt=""
+                />
+
+                {/* <p className="text-lg font-semibold">USA</p> */}
+
+                <button className="bg-red-300 text-red-700 font-semibold tracking-wider px-3 py-1 rounded-full text-center flex justify-center items-center hover:bg-red-400 hover:text-red-900">
+                  USA
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-10">
+            <div className="flex items-center gap-2">
+              <img className="w-8" src={calendar} alt="" />
+              <p className="font-semibold text-gray-500">
+                Sat, 3-Dec-2022, 8:45 PM
               </p>
-
-              <div className="flex gap-5 mt-10">
-                <div className="border border-gray-300  rounded-md p-5 flex flex-col gap-5 items-center w-1/2">
-                  <img
-                    className="w-20 h-20 object-cover object-center rounded-full"
-                    src={netherlands}
-                    alt=""
-                  />
-
-                  <p className="text-lg font-semibold">Netherlands</p>
-
-                  {/* <button className="bg-red-300 text-red-700 font-semibold tracking-wider px-3 py-1 rounded-full text-center flex justify-center items-center hover:bg-red-400 hover:text-red-900">
-                Netherlands
-              </button> */}
-                </div>
-
-                {/* <div className="w-20 h-20 rounded-full flex justify-center items-center font-semibold text-xl border shadow-sm">
-              X
-            </div> */}
-
-                <div className="border border-gray-300 rounded-md p-5 flex flex-col gap-5 items-center w-1/2">
-                  <img
-                    className="w-20 h-20 object-cover object-center rounded-full"
-                    src={usa}
-                    alt=""
-                  />
-
-                  <p className="text-lg font-semibold">USA</p>
-
-                  {/* <button className="bg-red-300 text-red-700 font-semibold tracking-wider px-3 py-1 rounded-full text-center flex justify-center items-center hover:bg-red-400 hover:text-red-900">
-                USA
-              </button> */}
-                </div>
-              </div>
             </div>
 
-            <div className="mt-10">
-              <div className="flex items-center gap-2">
-                <img className="w-8" src={calendar} alt="" />
-                <p className="font-semibold text-gray-500">
-                  Sat, 3-Dec-2022, 8:45 PM
-                </p>
-              </div>
-
-              <div className="flex items-center gap-2 mt-3">
-                <img className="w-8" src={stadium} alt="" />
-                <p className="font-semibold text-gray-500">
-                  Khalifa International Stadium
-                </p>
-              </div>
+            <div className="flex items-center gap-2 mt-3">
+              <img className="w-8" src={stadium} alt="" />
+              <p className="font-semibold text-gray-500">
+                Khalifa International Stadium
+              </p>
             </div>
+          </div>
 
-            <button className="bg-primary-500 text-white inline-block mt-5 w-full py-3 rounded-lg font-medium tracking-wide hover:bg-primary-400">
-              Predict
-            </button>
+          <button className="bg-primary-500 text-white inline-block mt-5 w-full py-3 rounded-lg font-medium tracking-wide hover:bg-primary-400">
+            Predict
+          </button>
 
-            {/* <FormFields /> */}
-          </form>
-        </div>
+          {/* <FormFields /> */}
+        </form>
 
         <img
           className="w-full aspect-auto  mt-10 lg:w-1/2 lg:-mt-5"
