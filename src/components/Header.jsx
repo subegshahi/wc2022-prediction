515a08d7ecf0f3fc3fe3f03bc33d7d9cb860c787
Subeg <84019079+subegshahi@users.noreply.github.com>
2022-12-02 20:35:14 +0545

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,14 @@ import navLogo from "../assets/images/chitrawan-khabar-logo.png";
 
 export const Header = () => {
   return (
-    <header className=" text-gray-800 border-b border-gray-200">
+    <header className=" text-gray-800 border-b border-gray-300">
       <div className="container mx-auto px-5 py-6 flex justify-between items-center lg:px-32">
         <img className="w-44" src={navLogo} alt="" />
 
         <nav className="space-x-10">
-          {/* <Link className="font-semibold tracking-wide" to="/">
+          <Link className="font-semibold tracking-wide" to="/">
             Home
-          </Link> */}
+          </Link>
 
           {/* <Link className="font-semibold tracking-wide" to="/login">
             Log in
@@ -21,9 +21,9 @@ export const Header = () => {
             Sign up
           </Link> */}
 
-          {/* <Link className="font-semibold tracking-wide" to="/predict">
+          <Link className="font-semibold tracking-wide" to="/predict">
             Predict
-          </Link> */}
+          </Link>
         </nav>
       </div>
     </header>
--- src/pages/Predict.jsx
+++ src/pages/Predict.jsx
@@ -1,23 +1,93 @@
 import React from "react";
+import { FormFields } from "../components/FormFields";
+import portugal from "../assets/images/portugal-flag.png";
+import argentina from "../assets/images/argentina-flag.png";
+import cup from "../assets/icons/world-cup.svg";
+import calendar from "../assets/icons/calendar.svg";
+import stadium from "../assets/icons/stadium.svg";
+import wc2022Logo from "../assets/icons/wc2022-logo.svg";
 
 export const Predict = () => {
   return (
     <main>
-      <div className="container mx-auto px-5 mt-10 lg:px-32">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptates
-          possimus iusto vero voluptatem doloribus minima officiis non ullam et
-          facere aut aliquid accusantium ipsa, cupiditate repellendus reiciendis
-          aperiam provident soluta numquam! Labore provident nostrum esse
-          tenetur incidunt ratione sequi, fugit magnam illum itaque ipsa sint
-          dolor voluptate eligendi rerum!This is new line
-        </p>
+      <div className="container mx-auto px-5 mt-20 lg:flex lg:px-32">
+        <div className="border border-gray-300 rounded-md p-10 lg:w-1/2">
+          <form action="">
+            <div>
+              <div>
+                <div className="flex items-center ">
+                  <img className="w-10" src={cup} alt="" />
+                  <p className="font-bold text-lg text-gray-800">
+                    World Cup 2022 - Round of 16
+                  </p>
+                </div>
+                <hr className="mt-3 border-gray-300" />
+              </div>
 
-        <p className="mt-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptates
-          possimus iusto vero voluptatem doloribus minima officiis non ullam et
-          facere aut aliquid accusantium ipsa, cupiditate repellendus reiciendis
-        </p>
+              <p className="mt-10 text-center text-3xl font-bold tracking-wide text-gray-800">
+                Who will win?
+              </p>
+
+              <div className="flex gap-5 mt-10">
+                <div className="border border-gray-300  rounded-md p-5 flex flex-col gap-5 items-center w-1/2">
+                  <img
+                    className="w-20 h-20 object-cover object-center rounded-full"
+                    src={portugal}
+                    alt=""
+                  />
+
+                  <p className="text-lg font-semibold">Netherlands</p>
+
+                  {/* <button className="bg-red-300 text-red-700 font-semibold tracking-wider px-3 py-1 rounded-full text-center flex justify-center items-center hover:bg-red-400 hover:text-red-900">
+                Netherlands
+              </button> */}
+                </div>
+
+                {/* <div className="w-20 h-20 rounded-full flex justify-center items-center font-semibold text-xl border shadow-sm">
+              X
+            </div> */}
+
+                <div className="border border-gray-300 rounded-md p-5 flex flex-col gap-5 items-center w-1/2">
+                  <img
+                    className="w-20 h-20 object-cover object-center rounded-full"
+                    src={argentina}
+                    alt=""
+                  />
+
+                  <p className="text-lg font-semibold">USA</p>
+
+                  {/* <button className="bg-red-300 text-red-700 font-semibold tracking-wider px-3 py-1 rounded-full text-center flex justify-center items-center hover:bg-red-400 hover:text-red-900">
+                USA
+              </button> */}
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-10">
+              <div className="flex items-center gap-2">
+                <img className="w-8" src={calendar} alt="" />
+                <p className="font-semibold text-gray-500">
+                  Sat, 3-Dec-2022, 8:45 PM
+                </p>
+              </div>
+
+              <div className="flex items-center gap-2 mt-3">
+                <img className="w-8" src={stadium} alt="" />
+                <p className="font-semibold text-gray-500">
+                  Khalifa International Stadium
+                </p>
+              </div>
+            </div>
+
+            {/* <FormFields /> */}
+          </form>
+        </div>
+
+        <img
+          className="w-full aspect-auto  mt-10 lg:w-1/2 lg:-mt-5"
+          src={wc2022Logo}
+          alt=""
+        />
       </div>
     </main>
   );
