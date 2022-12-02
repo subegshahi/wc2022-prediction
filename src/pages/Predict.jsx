3b8772eb95fdb85b81b1bf2c6f1774127f9b387b
Subeg <84019079+subegshahi@users.noreply.github.com>
2022-12-03 02:53:59 +0545

    Form Validation

--- src/pages/Predict.jsx
+++ src/pages/Predict.jsx
@@ -10,20 +10,42 @@ import stadium from "../assets/icons/stadium.svg";
 import wc2022Logo from "../assets/icons/wc2022-logo.svg";
 import { auth } from "../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export const Predict = () => {
   const [user] = useAuthState(auth);
 
+  const schema = yup.object().shape({
+    country: yup.string().required("Country is required"),
+    academicQualification: yup
+      .string()
+      .required("Academic Qualification is required"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmitForm = (data) => {
+    console.log(data);
+  };
+
   return (
     <main className="text-gray-800">
-      <div className="bg-gray-50 border-b border-gray-300 p-5">
+      <div className="bg-gray-50 border-b border-gray-300 px-5 py-3">
         <div className="container mx-auto lg:px-32">
-          <h1 className="font-semibold text-2xl">Welcome</h1>
+          <h1 className="font-semibold text-xl">Welcome</h1>
 
           <div className="flex items-center gap-2 mt-5">
             {user && (
               <>
-                <img src={user?.photoURL} className="w-14 rounded-full" />
+                <img src={user?.photoURL} className="w-10 rounded-full" />
                 <p className="font-bold"> {user?.displayName} </p>
               </>
             )}
@@ -34,7 +56,7 @@ export const Predict = () => {
       <div className="container mx-auto px-5 mt-5 lg:flex lg:px-32">
         <form
           className="border border-gray-300 rounded-md p-10 mt-10 lg:w-1/2"
-          action=""
+          onSubmit={handleSubmit(onSubmitForm)}
         >
           <div>
             <div>
@@ -59,11 +81,11 @@ export const Predict = () => {
                   alt=""
                 />
 
-                {/* <p className="text-lg font-semibold">Netherlands</p> */}
+                <p className="text-lg font-semibold">Netherlands</p>
 
-                <button className="bg-red-300 text-red-700 font-semibold tracking-wider px-3 py-1 rounded-full text-center flex justify-center items-center hover:bg-red-400 hover:text-red-900">
+                {/* <button className="bg-red-300 text-red-700 font-semibold tracking-wider px-3 py-1 rounded-full text-center flex justify-center items-center hover:bg-red-400 hover:text-red-900">
                   Netherlands
-                </button>
+                </button> */}
               </div>
 
               {/* <div className="w-20 h-20 rounded-full flex justify-center items-center font-semibold text-xl border shadow-sm">
@@ -77,14 +99,65 @@ export const Predict = () => {
                   alt=""
                 />
 
-                {/* <p className="text-lg font-semibold">USA</p> */}
+                <p className="text-lg font-semibold">USA</p>
 
-                <button className="bg-red-300 text-red-700 font-semibold tracking-wider px-3 py-1 rounded-full text-center flex justify-center items-center hover:bg-red-400 hover:text-red-900">
+                {/* <button className="bg-red-300 text-red-700 font-semibold tracking-wider px-3 py-1 rounded-full text-center flex justify-center items-center hover:bg-red-400 hover:text-red-900">
                   USA
-                </button>
+                </button> */}
               </div>
             </div>
           </div>
+
+          <div className="mt-5">
+            <label className="block">Country</label>
+
+            <input
+              className="w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:border-primary-500 focus:ring-1 focus:ring-primary-500"
+              type="text"
+              placeholder="Enter country name"
+              {...register("country")}
+            />
+
+            <p className="mt-2 text-red-700">{errors.country?.message}</p>
+          </div>
+
+          <div className="mt-5">
+            <label className="block">Academic Qualification</label>
+
+            <input
+              className="w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus:border-primary-500 focus:ring-1 focus:ring-primary-500"
+              type="text"
+              placeholder="Enter your academic qualification"
+              {...register("academicQualification")}
+            />
+
+            <p className="mt-2 text-red-700">
+              {errors.academicQualification?.message}
+            </p>
+          </div>
+
+          {/* <select
+            id="countries"
+            className="w-full py-3 px-3 border border-gray-300 rounded-md focus:outline-none focus:border-primary-500/50 focus:ring-1 focus:ring-primary-500/50 mt-5 font-semibold"
+          >
+            <option selected>Choose a country</option>
+            <option>Netherlands</option>
+            <option>USA</option>
+          </select> */}
+
+          {/* <select
+            id="academicQualification"
+            className="w-full py-3 px-3 border border-gray-300 rounded-md focus:outline-none focus:border-primary-500/50 focus:ring-1 focus:ring-primary-500/50 mt-5 font-semibold"
+          >
+            <option selected>Choose your academic qualification</option>
+            <option>SEE</option>
+            <option>+2</option>
+            <option>Bachelor's Degree</option>
+            <option>Master's Degree</option>
+            <option>Doctorate</option>
+          </select> */}
+
+          {/* <FormFields /> */}
 
           <div className="mt-10">
             <div className="flex items-center gap-2">
@@ -102,11 +175,12 @@ export const Predict = () => {
             </div>
           </div>
 
-          <button className="bg-primary-500 text-white inline-block mt-5 w-full py-3 rounded-lg font-medium tracking-wide hover:bg-primary-400">
+          <button
+            className="bg-primary-500 text-white inline-block mt-5 w-full py-3 rounded-lg font-medium tracking-wide hover:bg-primary-400"
+            type="submit"
+          >
             Predict
           </button>
-
-          {/* <FormFields /> */}
         </form>
 
         <img
